9c7189398aecb445eba4ea15560cf9bbc8ce9d89
EAinsley <vhtmscyo@gmail.com>
2022-10-11 21:28:50 +0800

    FIX(thread.c): fix recalculation priority after unblocking the thread.

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -255,6 +255,7 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
+  t->priority = mlfqs_calculate_priority (t);
   list_insert_ordered (&ready_list, &t->elem, thread_priority_more, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
