a419bae4a01deb5d1a3b1d02d891a4298f9c8ac8
hehelego <2364261262@qq.com>
2022-10-26 11:18:30 +0800

    upd(syscall.c): temporarily add write-syscall impl for arg-pass

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <syscall-nr.h>
 
+#define syscall_arg(frame, idx, type) (*(type *)((char *)frame->esp + 4 * idx))
+
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -12,9 +14,26 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+// int syscall_write (int fd, const void *buffer, unsigned size);
+
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
+  // temporarily added for arg-pass
+  if (*(int *)f->esp == SYS_WRITE)
+    {
+      int fd = syscall_arg (f, 1, int);
+      char *buf = syscall_arg (f, 2, char *);
+      unsigned size = syscall_arg (f, 3, unsigned);
+
+      if (fd == 1)
+        {
+          for (unsigned i = 0; i < size; i++)
+            putchar (buf[i]);
+          return;
+        }
+    }
+
+  // printf ("system call!\n");
   thread_exit ();
 }
