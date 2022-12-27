b7db8a627d15ac60fa2a8cad378a75053fa3c814
hehelego <2364261262@qq.com>
2022-12-27 17:56:18 +0800

    upd(inode): simplify code

--- src/filesys/inode.h
+++ src/filesys/inode.h
@@ -20,4 +20,8 @@ void inode_deny_write (struct inode *);
 void inode_allow_write (struct inode *);
 off_t inode_length (const struct inode *);
 
+/* directory tree support */
+block_sector_t inode_getdir(const struct inode *);
+void inode_setdir(struct inode *, block_sector_t);
+
 #endif /* filesys/inode.h */
