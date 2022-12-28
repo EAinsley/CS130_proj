c37d34f93aa82b774e53e2d2681757590818268c
hehelego <2364261262@qq.com>
2022-12-28 11:46:16 +0800

    fix(fs): in `filesys_create` parameter for `inode_create`

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -74,7 +74,8 @@ filesys_create (const char *name, off_t initial_size, bool is_dir)
   block_sector_t inode_sector = 0;
   struct dir *dir = dir_open_path (path);
   bool success = (dir != NULL && free_map_allocate (1, &inode_sector)
-                  && inode_create (inode_sector, initial_size, is_dir, dir)
+                  && inode_create (inode_sector, initial_size, is_dir,
+                                   inode_get_inumber (dir_get_inode (dir)))
                   && dir_add (dir, dirname, inode_sector));
   if (!success && inode_sector != 0)
     free_map_release (inode_sector, 1);
