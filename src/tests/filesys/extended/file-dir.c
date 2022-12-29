fea380c067c2f265e19a51936d7408cf4eea9d45
hehelego <2364261262@qq.com>
2022-12-29 12:30:16 +0800

    upd(tests/file-dir): enhance testcase

--- src/tests/filesys/extended/file-dir.c
+++ src/tests/filesys/extended/file-dir.c
@@ -10,4 +10,10 @@ test_main (void)
   CHECK (!create (".", 0), "create . must fail");
   CHECK (!create ("a/", 0), "create a/ fail");
   CHECK (!create ("b//", 0), "create b// fail");
+
+  CHECK (mkdir ("hello"), "mkdir hello ok");
+  CHECK (!mkdir ("hello/./"), "mkdir hello/./ should fail");
+
+  CHECK (create ("hello/a", 0), "create hello/a ok");
+  CHECK (!create ("hello/./a", 0), "create hello/./a fail");
 }
--- src/tests/filesys/extended/file-dir.ck
+++ src/tests/filesys/extended/file-dir.ck
@@ -8,6 +8,10 @@ check_expected ([<<'EOF']);
 (file-dir) create . must fail
 (file-dir) create a/ fail
 (file-dir) create b// fail
+(file-dir) mkdir hello ok
+(file-dir) mkdir hello/./ should fail
+(file-dir) create hello/a ok
+(file-dir) create hello/./a fail
 (file-dir) end
 file-dir: exit(0)
 EOF
